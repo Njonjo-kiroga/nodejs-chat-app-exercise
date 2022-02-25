3cc988c15802a8573bd8d8f1073a861400de8a9d
Njonjo Kiroga <andrewnjonjo@gmail.com>
2022-02-25 16:15:56 +0300

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.static(__dirname))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 mongoose.Promise = Promise 
-var dbUrl = 'mongodb+srv://Njonjo:ANKiroga23@cluster0.crg2p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+var dbUrl =
 
 var Message = mongoose.model('Message', {
     name: String,
